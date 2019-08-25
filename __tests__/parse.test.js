1cfb6905c2de59f27f2a900ead6cf42196580908
Cody Price <34115417+dev-cprice@users.noreply.github.com>
2019-08-25 16:00:58 -0500

    fix default + star imports case, sideEffectOnly check (#5)

--- __tests__/parse.test.js
+++ __tests__/parse.test.js
@@ -2,8 +2,10 @@ import fs from "fs";
 import path from "path";
 import { promisify } from "util";
 import parse from "../src/parse";
+import importSyntaxList from "./__fixtures__/importSyntaxList";
 
 const readFileAsync = promisify(fs.readFile);
+const fixturesDirectoryPath = path.resolve(__dirname, "__fixtures__");
 
 test("when given nothing returns an empty list", () => {
   const expected = [];
@@ -12,21 +14,31 @@ test("when given nothing returns an empty list", () => {
   expect(result).toStrictEqual(expected);
 });
 
-test("it matches the snapshot for each fixture", async () => {
-  const fixturesDirectoryPath = path.resolve(__dirname, "__fixtures__");
+describe("for each example in the import syntax list it returns the expected shape", () => {
+  importSyntaxList.forEach(({ actual, expected }) => {
+    const result = parse(actual);
 
-  const fixturePaths = fs
-    .readdirSync(fixturesDirectoryPath)
-    .map(filename => path.join(fixturesDirectoryPath, filename));
+    it(actual, () => {
+      expect(result).toStrictEqual(expected);
+    });
+  });
+});
 
-  const fixtures = await Promise.all(
-    fixturePaths.map(fixturePath => readFileAsync(fixturePath, "utf8"))
+test("it matches the snapshot for each app example fixture", async () => {
+  const appExamplesPath = path.resolve(fixturesDirectoryPath, "app-examples");
+
+  const appExamplePaths = fs
+    .readdirSync(appExamplesPath)
+    .map(filename => path.join(appExamplesPath, filename));
+
+  const appExamples = await Promise.all(
+    appExamplePaths.map(fixturePath => readFileAsync(fixturePath, "utf8"))
   );
 
-  fixtures.forEach((fixture, index) => {
+  appExamples.forEach((fixture, index) => {
     const result = {
       result: parse(fixture),
-      fixtureName: path.parse(fixturePaths[index]).base
+      name: path.parse(appExamplePaths[index]).base
     };
 
     expect(result).toMatchSnapshot();
--- src/isSideEffectOnly.js
+++ src/isSideEffectOnly.js
@@ -2,10 +2,9 @@ export default function isSideEffectOnly(str = "") {
   const importIdx = str.indexOf("import");
   const quoteIdx = str.replace(/(\'|\`)+/gm, '"').indexOf('"'); // eslint-disable-line no-useless-escape
 
-  const semiColonIdx = str.indexOf(";");
   const startIdx = importIdx >= 0 ? importIdx + 6 : 0;
 
   const betweenImportAndQuote = str.substring(startIdx, quoteIdx).trim();
 
-  return semiColonIdx >= 0 && betweenImportAndQuote.length === 0;
+  return importIdx >= 0 && betweenImportAndQuote.length === 0;
 }
